e623e51d58b6ff9c7e9e5d18bc3c31ff7940c37b
Quoc Cuong <quoccuong14012000@gmail.com>
2025-05-19 14:59:52 +0700

    Update: add bootstrap

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,10 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: { alias: { '@': (path.resolve(__dirname, './src/')) } },
+  resolve: {
+    alias: {
+      '@': (path.resolve(__dirname, './src/')),
+      '@axios': (path.resolve(__dirname, './src/bootstrap/api-interceptor/')),
+    },
+  },
 });
